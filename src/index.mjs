46bbb3b4c9815a10853c103e60dc054b01c01d07
Your Name <your_email@example.com>
2024-04-12 22:18:24 +1000

    docker implement

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**@param {string} a @returns {string} */
 function some(a) {
 	return a
@@ -5,3 +6,16 @@ function some(a) {
 some(0)
 	
 let a
+=======
+import { createServer } from 'http';
+
+const port = 8000;
+
+const server = createServer((req, res) => {
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running`);
+});
+>>>>>>> 9c69666 (docker implement)
