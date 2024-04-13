932f4b3e278c1b6350f8c72ba3d1c907161772de
Your Name <your_email@example.com>
2024-04-13 11:05:31 +1000

    d

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,11 @@
-import { createServer } from 'http';
+import { createServer } from 'http'
 
-const port = 8000;
+const port = 8000
 
 const server = createServer((req, res) => {
-  res.end('Hello, World!\n');
-});
+	res.end('Hello, World!\n')
+})
 
 server.listen(port, () => {
-  console.log(`Server running`);
-});
+	console.log(`Server running`)
+})
